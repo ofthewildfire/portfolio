da73b74d4768c483dfc4d8d49b7ed2c613db601d
ofthewildfire <lissajace@gmail.com>
2022-10-21 23:31:29 +0200

    fixed all  pending accessibility issues

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const flipcard1 = document.getElementById("flipcard1");
-const flipcard2 = document.getElementById("flipcard2");
-const flipcard3 = document.getElementById("flipcard3");
 
-const projects = document.querySelectorAll(".project-article");
-
-flipcard1.addEventListener("mouseover", function () {
- projects.style.display = "none";
-});
